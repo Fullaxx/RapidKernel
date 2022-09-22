255028486f5d1bc78b8a61de773414cd22a1f791
Brett Kuskie <fullaxx@gmail.com>
2022-09-22 10:57:02 -0600

    Split 02a-make_kernel.sh from 02-make.sh

--- scripts/02-make.sh
+++ scripts/02-make.sh
@@ -19,22 +19,8 @@ if [ "${SCRLOC}" == "." ]; then
   SCRLOC=`pwd`
 fi
 
-cd /usr/src/linux || bail "cd /usr/src/linux failed!"
-
-# Error Checking
-if [ ! -d ${KSAVEDIR} ]; then bail "${KSAVEDIR} not found!"; fi
-if [ ! -r .config ]; then bail ".config not found!"; fi
-
-cp -v .config ${KSAVEDIR}/config || bail "cp -v .config ${KSAVEDIR}/config failed!"
-make -j `nproc` || bail "Error during make"
-
-# Make modules before cleaning up the source tree.
-# This does some magic in Module.symvers that fixes building the NVIDIA kernel modules.
-make -j `nproc` modules || bail "Error during make modules"
-echo
-
-cp -v arch/x86/boot/bzImage Module.symvers System.map ${KSAVEDIR}/ || bail "cp -v arch/x86/boot/bzImage Module.symvers System.map ${KSAVEDIR}/ failed!"
-echo
+cd ${SCRLOC}
+./02a-make_kernel.sh ${KSAVEDIR}
 
 cd ${SCRLOC}
 ./02b-make_headers.sh ${KSAVEDIR}
---
+++ scripts/02a-make_kernel.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "$0 <output directory>"
+  exit 1
+fi
+
+if [ -r ./vars.sh ]; then
+  source ./vars.sh
+else
+  >&2 echo "./vars.sh does not exist!"
+  exit 1
+fi
+
+KSAVEDIR="$1"
+
+cd /usr/src/linux || bail "cd /usr/src/linux failed!"
+
+# Error Checking
+if [ ! -d ${KSAVEDIR} ]; then bail "${KSAVEDIR} not found!"; fi
+if [ ! -r .config ]; then bail ".config not found!"; fi
+
+cp -v .config ${KSAVEDIR}/config || bail "cp -v .config ${KSAVEDIR}/config failed!"
+make -j `nproc` || bail "Error during make"
+echo
+
+# Make modules before cleaning up the source tree.
+# This does some magic in Module.symvers that fixes building the NVIDIA kernel modules.
+make -j `nproc` modules || bail "Error during make modules"
+echo
+
+for FILE in arch/x86/boot/bzImage Module.symvers System.map; do
+  cp -v ${FILE} ${KSAVEDIR}/ || bail "cp -v ${FILE} ${KSAVEDIR}/ failed!"
+done
+echo
